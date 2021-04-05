ad25c4883208ff603e182ba49433c79f81c4829f
Vital Ozierski <v.ozierski@ozitag.com>
2021-04-05 19:13:26 +0300

    add UrlHelper

---
+++ src/Helpers/UrlHelper.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace OZiTAG\Tager\Backend\Utils\Helpers;
+
+class UrlHelper
+{
+    public static function isAbsoluteUrl(string $url): bool
+    {
+        $pattern = "/^(?:ftp|https?|feed)?:?\/\/(?:(?:(?:[\w\.\-\+!$&'\(\)*\+,;=]|%[0-9a-f]{2})+:)*
+        (?:[\w\.\-\+%!$&'\(\)*\+,;=]|%[0-9a-f]{2})+@)?(?:
+        (?:[a-z0-9\-\.]|%[0-9a-f]{2})+|(?:\[(?:[0-9a-f]{0,4}:)*(?:[0-9a-f]{0,4})\]))(?::[0-9]+)?(?:[\/|\?]
+        (?:[\w#!:\.\?\+\|=&@$'~*,;\/\(\)\[\]\-]|%[0-9a-f]{2})*)?$/xi";
+
+        return (bool)preg_match($pattern, $url);
+    }
+}
