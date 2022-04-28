b91d17d1270adb9e50bfaf3815fa2b57ce6d9ab8
Vital Ozierski <v.ozierski@ozitag.com>
2022-04-28 17:27:42 +0300

    fix stringHelper

--- src/Helpers/StringHelper.php
+++ src/Helpers/StringHelper.php
@@ -10,9 +10,4 @@ class StringHelper
         json_decode($string);
         return json_last_error() === JSON_ERROR_NONE;
     }
-
-    public static function getHumanDateTime($timestamp = null)
-    {
-        return date('Y-m-d H:i:s', $timestamp ? $timestamp : time());
-    }
 }
