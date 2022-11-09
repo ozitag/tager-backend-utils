d9c193caadc9f54e46e25a90957d472f253eba2b
Vital Ozierski <vital@ozerich.com>
2022-11-09 11:53:00 +0300

    add RequestHelper

---
+++ src/Helpers/RequestHelper.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace OZiTAG\Tager\Backend\Utils\Helpers;
+
+class RequestHelper
+{
+    public function getIpAddress(): ?string
+    {
+        if (getenv('HTTP_CLIENT_IP'))
+            return getenv('HTTP_CLIENT_IP');
+        else if(getenv('HTTP_X_FORWARDED_FOR'))
+            return getenv('HTTP_X_FORWARDED_FOR');
+        else if(getenv('HTTP_X_FORWARDED'))
+            return getenv('HTTP_X_FORWARDED');
+        else if(getenv('HTTP_FORWARDED_FOR'))
+            return getenv('HTTP_FORWARDED_FOR');
+        else if(getenv('HTTP_FORWARDED'))
+            return getenv('HTTP_FORWARDED');
+        else if(getenv('REMOTE_ADDR'))
+            return getenv('REMOTE_ADDR');
+
+        return null;
+    }
+}
