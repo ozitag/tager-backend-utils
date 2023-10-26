a5d777c2357d676b677fc33c60dc7757ec1ce8fc
Vital Ozierski <vital@ozerich.com>
2023-10-26 12:40:15 +0200

    fix isJson helper string

--- src/Helpers/StringHelper.php
+++ src/Helpers/StringHelper.php
@@ -7,6 +7,9 @@ class StringHelper
     public static function isJson($string): bool
     {
         $string = (string)$string;
+
+        if(is_numeric($string)) return false;
+
         json_decode($string);
         return json_last_error() === JSON_ERROR_NONE;
     }
